cc7d324a45ed67b42cb8f729e26f2e4c210e1eaa
Mohammed Khallaf <mohammedkhallaf.dev@gmail.com>
2024-09-29 19:37:43 +0300

    initial setup

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 // entry point
+// express -> server ( request | response )
+
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log(req);
+
+  res.json({
+    message: "Hello World!",
+  });
+  // .json({})
+  // .status(XXX)
+});
+
+app.listen(3000, () => {
+  console.log(`Example app listening on port 3000`);
+});
